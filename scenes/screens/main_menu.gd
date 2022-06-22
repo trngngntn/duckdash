81164deaa8ad0d5084ff3aaf16df90aa6c39ba30
sirclex <lethanhluan2000@gmail.com>
2022-06-23 00:45:38 +0700

    Add Main Menu layout

---
+++ scenes/screens/main_menu.gd
@@ -0,0 +1,34 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if Conn.nkm_session == null:
+		get_tree().change_scene("res://scenes/screens/sign_in.tscn")
+	else:
+		$GreetingRichTextLabel.text = "Hello, " + Conn.nkm_session.username
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_PlayButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_InventoryButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_MarketplaceButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
