2150b87a1cc2733b2c4c89524282e587352be348
Truong <trngngntn@gmail.com>
2022-07-29 16:16:20 +0700

    ui updates

--- scripts/screens/main_menu.gd
+++ scripts/screens/main_menu.gd
@@ -1,35 +1,34 @@
 extends Control
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(Conn.nkm_session)
 	if Conn.nkm_session == null:
 		ScreenManager.change_screen(ScreenManager.SCREEN_LOGIN)
-	else:
-		$MarginContainer/HBoxContainer/VBoxContainer2/HBoxContainer/GreetingLabel.text = "Hello, " + Conn.nkm_session.username
+		# else:
+		# 	$MarginContainer/HBoxContainer/VBoxContainer2/HBoxContainer/GreetingLabel.text = "Hello, " + Conn.nkm_session.username
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_ButtonExit_pressed():
+	get_tree().quit()
 
 
-func _on_PlayButton_pressed():
-	ScreenManager.change_screen(ScreenManager.SCREEN_LOBBY)
+func _on_ButtonSettings_pressed():
+	pass  # Replace with function body.
 
 
-func _on_InventoryButton_pressed():
-	ScreenManager.change_screen(ScreenManager.SCREEN_INVENTORY)
-
-
-func _on_MarketplaceButton_pressed():
+func _on_ButtonMarketplace_pressed():
 	ScreenManager.change_screen(ScreenManager.SCREEN_MARKETPLACE)
 
 
-func _on_QuitButton_pressed():
-	get_tree().quit()
+func _on_ButtonInventory_pressed():
+	ScreenManager.change_screen(ScreenManager.SCREEN_INVENTORY)
+
+
+func _on_ButtonPlay_pressed():
+	ScreenManager.change_screen(ScreenManager.SCREEN_LOBBY)
