2eeb2d881d3166554638432078be643b7e31bfa2
Truong <trngngntn@gmail.com>
2022-08-14 00:38:12 +0700

    fix null camera instance

--- scripts/screens/in_game/game_camera.gd
+++ scripts/screens/in_game/game_camera.gd
@@ -2,13 +2,14 @@ extends Camera2D
 
 var tween: Tween
 var tracking_node : Node
+var set: bool = false
 
 func _ready() -> void:
 	tween = Tween.new()
 	add_child(tween)
 
 func _physics_process(delta) -> void:
-	if not is_instance_valid(tracking_node) || tracking_node.is_queued_for_deletion():
+	if set && (not is_instance_valid(tracking_node) || tracking_node.is_queued_for_deletion()):
 		queue_free()
 		return
 	if tracking_node:
@@ -26,6 +27,7 @@ func _physics_process(delta) -> void:
 func set_node_tracking(node: Node) -> void:
 	print("CAM_TRACK: " + str(node)) 
 	tracking_node = node
+	set = true
 	# if tracking_node.has_method("set_tracking_cam"):
 	# 	tracking_node.set_tracking_cam(self)
 	tween.start()
